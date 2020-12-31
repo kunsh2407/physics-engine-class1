ed1eb8ad9abe2051c65c2ea68f52aba9fe415436
kunsh2407 <75424504+kunsh2407@users.noreply.github.com>
2020-12-31 21:02:05 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,27 @@
+class Box{
+    constructor(x,y,width,height){
+        var options ={
+             restitution:1
+        }
+    
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        World.add(world,this.body);
+    this.width=width;
+    this.height=height;
+    
+
+    }
+    display(){
+        var pos=this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        fill("white");
+        stroke("green");
+        strokeWeight(5);
+        rectMode(CENTER);
+    rect(0,0,this.width,this.height);
+    pop();
+    }
+}    
